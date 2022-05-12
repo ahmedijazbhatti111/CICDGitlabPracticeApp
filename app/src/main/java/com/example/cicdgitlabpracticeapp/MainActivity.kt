7c603b21cf4c8d3e302094c9bf4a4ba7f582ab8b
ahmad.ijaz <mailto:ahmed.ijazi@safarifoneict.com>
2022-05-12 14:46:38 +0500

    initial commit add something 5

--- app/src/main/java/com/example/cicdgitlabpracticeapp/MainActivity.kt
+++ app/src/main/java/com/example/cicdgitlabpracticeapp/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("","123")
+        Log.d("","1234")
     }
 }
