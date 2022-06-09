73ea2781cc0404951bac113426622a1056cd9d43
ahmad.ijaz <mailto:ahmed.ijazi@safarifoneict.com>
2022-06-09 17:41:51 +0500

    Initial commit

---
+++ app/src/main/java/com/example/cicdgitlabpracticeapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.cicdgitlabpracticeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("","123456789")
+    }
+}
